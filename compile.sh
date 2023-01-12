758c5e1a7965e5cdeb4ef234f50777537854bb00
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-01-12 00:18:52 -0500

    add custom tlc

---
+++ compile.sh
@@ -0,0 +1,7 @@
+cd /Users/paulo.feodrippe/dev/tlaplus/tlatools/org.lamport.tlatools
+ant -f customBuild.xml -Dtest.skip=true
+cd -
+
+cp /Users/paulo.feodrippe/dev/tlaplus/tlatools/org.lamport.tlatools/dist/tla2tools.jar jars/tla2tools.jar
+make build
+make deploy
