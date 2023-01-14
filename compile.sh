e8ffac7954fb9b58f64157ddc14354e43dcba1ee
Paulo Feodrippe <paulo.feodrippe@Paulos-MacBook-Pro.local>
2023-01-13 23:02:20 -0500

    revert RecifeEdnValue

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
+# cp ~/dev/recife/classes/recife/RecifeEdnValue.class \
+#   ~/dev/tlaplus/tlatools/org.lamport.tlatools/lib/recife/RecifeEdnValue.class
+
 cd /Users/paulo.feodrippe/dev/tlaplus/tlatools/org.lamport.tlatools
 ant -f customBuild.xml -Dtest.skip=true
 cd -
